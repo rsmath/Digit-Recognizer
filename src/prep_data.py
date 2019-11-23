4d4f77685e22a3c17e5c8bb6fe843b141b2cdf5f
Ramansh Sharma <sharmar@bxscience.edu>
2019-11-23 14:58:57 +0530

    labels variable created

--- src/prep_data.py
+++ src/prep_data.py
@@ -8,14 +8,18 @@ import pandas as pd
 
 data = pd.read_csv('../digit-recognizer/train.csv')
 
+labels = data['label']
+
+data.set_index('label', inplace=True) # permanently sets label column to be the index
+
 train_data = np.asarray(data)
 test_data = np.asarray(pd.read_csv('../digit-recognizer/test.csv'))
 
-print(data.iloc[0])
-
+print(train_data.shape) # (42000, 784)
 
 '''
 FOR NOW THERE WILL BE NO CROSS VALIDATION TEST. AFTER A PRELIMINARY MODEL HAS BEEN MADE AND TESTED, 
 THEN A CROSS VALIDATION TEST WILL BE USED TO OPTIMIZE THE REGULARIZATION PARAMETER (THERE IS NO
 REGULARIZATION RIGHT NOW EITHER)
 '''
+
