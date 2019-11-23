9b442acbcfa3172a04883faa501a23954485a5b9
Ramansh Sharma <sharmar@bxscience.edu>
2019-11-23 15:12:12 +0530

    variable y is made with labels

--- src/compute_cost.py
+++ src/compute_cost.py
@@ -3,11 +3,19 @@ This module will compute the cost of each iteration of forward prop
 """
 
 import numpy as np
+from src.prep_data import labels
 
+y = []
 
-def compute_cost(AL, y):
+for i in range(len(labels)):
+    temp = np.zeros((10,), dtype=int)
+    temp[labels[i]] = 1
+    y.append(temp)
 
+y = np.asarray(y) # shape (42000, 10), a 1 for each label digit's position in an empty (10,) zeros array
 
+def compute_cost(AL):
+    pass
 
 
 
--- src/prep_data.py
+++ src/prep_data.py
@@ -8,14 +8,14 @@ import pandas as pd
 
 data = pd.read_csv('../digit-recognizer/train.csv')
 
-labels = data['label']
+labels = np.asarray(data['label']) # can be used as y, the true values
 
 data.set_index('label', inplace=True) # permanently sets label column to be the index
 
 train_data = np.asarray(data)
 test_data = np.asarray(pd.read_csv('../digit-recognizer/test.csv'))
 
-print(train_data.shape) # (42000, 784)
+# print(train_data.shape) # (42000, 784)
 
 '''
 FOR NOW THERE WILL BE NO CROSS VALIDATION TEST. AFTER A PRELIMINARY MODEL HAS BEEN MADE AND TESTED, 
