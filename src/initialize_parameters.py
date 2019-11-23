d714362c2f79fc3f0fd8bbf5df3aa56d1453ae2c
Ramansh Sharma <sharmar@bxscience.edu>
2019-11-23 13:45:04 +0530

    code cleanup

--- src/initialize_parameters.py
+++ src/initialize_parameters.py
@@ -4,15 +4,14 @@ The weights, W, and the biases, b, will be generated for the neural network's la
 """
 
 import numpy as np
-from src.model import layers # importing the layers of the different hidden layers
-
-L = len(layers)
 
 parameters = {}
 
 def initialize_parameters(layers):
     # this function will be imported in model to initialize paramters
     # returns parameters
+
+    L = len(layers)
 
     for i in range(1, L):
         parameters['W' + str(i)] = np.random.randn(layers[i], layers[i - 1]) * 0.01
