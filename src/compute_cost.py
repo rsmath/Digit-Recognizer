some changes based on softmax insertion (correct equation this time)

src/compute_cost.py
@@ -15,7 +15,7 @@ def compute_cost(AL, y):
     :return: cost of the function L
     """
 
-    cost = -np.mean(y * np.log(AL + 1e-8)) # 1e-8 added to avoid dividing by 0
+    cost = (-1 / m) * np.mean(y * np.log(AL + 1e-8)) # 1e-8 added to avoid taking log of 0
     cost = np.squeeze(cost) # turns [[17]] into 17
 
     assert (cost.shape == ())

src/equations.py
@@ -25,20 +25,16 @@ def softmax(Z):
     return A, cache
 
 
-def softmax_backward(dA, cache, y):
+def softmax_backward(AL, cache, y):
     """
-    computing dZ
-    :param dA: dA of current layer
-    :param cache: Z from softmax
+    computes dZ for softmax
+    :param cache: Z value
+    :param AL: output layer A value
+    :param y: ground truth
     :return: dZ
     """
-    AL = dA # dA is actually AL
 
     Z = cache
-
-    # e_Z = np.exp(Z - np.max(Z))
-    #
-    # s = e_Z / e_Z.sum(axis=0)
 
     dZ = AL - y  # derivative of cost with respect to Z for softmax function
 

src/main.py
@@ -3,23 +3,24 @@ This module is just to run the model in a clean environment with new data
 """
 
 from src.model import VanillaNN
-from src.prep_data import test_data
+from src.prep_data import test_data, train_data
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-layers = [784, 35, 20, 10]
+layers = [784, 35, 25, 20, 10]
 
-model = VanillaNN(layer_dims=layers, iterations=100, learning_rate=0.075, print_cost=True)
+model = VanillaNN(layer_dims=layers, iterations=100, learning_rate=0.1, print_cost=True)
 
 if __name__ == "__main__":
     costs = model.train()
-    X_test = test_data[:, np.random.randint(400)].reshape(784, 1) # random image
+    X_test = test_data[:, np.random.randint(28000)].reshape(784, 1) # random image
     output = model.test(X_test)
     print(output)
     digit = np.where(output == np.amax(output))[0][0]
     fig = np.asarray(X_test).reshape(28, 28)
     plt.title(f"The test example digit is: {digit}")
+    # fig = plt.plot(costs)
     plt.imshow(fig)
     plt.show()
 