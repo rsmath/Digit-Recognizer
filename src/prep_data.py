11ce387b47a620b22ebc31378f9b358e7aa1311c
Ramansh Sharma <sharmar@bxscience.edu>
2019-12-04 14:30:29 +0530

    small changes for better experience and further clarity while using the model

--- src/prep_data.py
+++ src/prep_data.py
@@ -15,15 +15,15 @@ data.set_index('label', inplace=True)  # permanently sets label column to be the
 
 data_original = np.asarray(data)
 data_original = np.transpose(data_original)  # this fixes a small bug, changes shape to (784, 42000)
-train_data = data_original[:, : 25200]  # (784, 25200) since test.csv does not have any labels, 25200 of train is the
+train_data = data_original[:, : 29400]  # (784, 29400) since test.csv does not have any labels, 29400 of train is the
 # training set, remaining is test set
 
-labels_train = labels[: 25200] # labels for training set
+labels_train = labels[: 29400] # labels for training set
 
 
-test_data = data_original[:, 25200:]  # shape (784, 16800)
+test_data = data_original[:, 29400:]  # shape (784, 12600)
 
-labels_test = labels[25200:] # labels for test set
+labels_test = labels[29400:] # labels for test set
 
 m_train = len(labels_train)  # number of training examples in training data
 m_test = len(labels_test) # number of examples in test set
@@ -34,8 +34,8 @@ for i in range(m_train):
     temp[labels_train[i]] = 1
     y.append(temp)
 
-y = np.asarray(y)  # shape (25200, 10), a 1 for each label digit's position in an empty (10,) zeros array
-y = np.transpose(y)  # fixes a bug and changes shape of y to (10, 25200)
+y = np.asarray(y)  # shape (29400, 10), a 1 for each label digit's position in an empty (10,) zeros array
+y = np.transpose(y)  # fixes a bug and changes shape of y to (10, 29400)
 
 '''
 FOR NOW THERE WILL BE NO CROSS VALIDATION TEST. AFTER A PRELIMINARY MODEL HAS BEEN MADE AND TESTED, 
